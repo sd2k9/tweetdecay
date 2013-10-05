0f232bc8fac0a2f6a3c4ab986d1590a4787688ea
sd2k9 <sd2k9@sethdepot.org>
2013-10-05 15:18:37 +0200

    Author Mail update

--- tweetdecay.py
+++ tweetdecay.py
@@ -7,7 +7,7 @@
 #
 # last change: $LastChangedRevision$
 #
-#    Copyright (C) 2010,2011 Robert Lange (robert.lange@s1999.tu-chemnitz.de)
+#    Copyright (C) 2010,2011 Robert Lange <sd2k9@sethdepot.org>
 #    This program is free software; you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation; either version 2 of the License, or
